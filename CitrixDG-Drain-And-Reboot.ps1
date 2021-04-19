0c810523072347389cd37a7ada66edcc7162fa03
Megan <8890846+MeganFuhr@users.noreply.github.com>
2021-04-19 13:37:16 -0400

    Update CitrixDG-Drain-And-Reboot.ps1
    Changed to target 20% of a delivery group to place in maintenance mode.  Can be updated to loop through DDCs/DGs.

--- CitrixDG-Drain-And-Reboot.ps1
+++ CitrixDG-Drain-And-Reboot.ps1
@@ -1,57 +1,111 @@
-#############################################################################################################
-#   Author: Megan Fuhr
-#   Description: This script will tag a server in a delivery group that
-#                is not in maintenance mode with the word DRAIN and place in maintenance mode.
-#                Upon next run, it checks for a tagged server and if it is in maintenance mode 
-#                and has no sessions, the tag will be removed, server taken out of maintenance mode,
-#                and rebooted.  It will tag a new server if none has already been tagged.
-#                Only one server will be tagged and placed in maintenance mode by this script 
-#                at a time.
-#############################################################################################################
-
 try {
     Add-PSSnapin Citrix.* -ErrorAction SilentlyContinue
-}
-catch {RETURN}
-
-#Variables
-$DDC = "YourDDC.Domain.com"
-$deliverygroups = Get-BrokerDesktopGroup -AdminAddress $DDC -MaxRecordCount 10000 -Filter {SessionSupport -eq "MultiSession"}
-
-
-foreach ($deliverygroup in $deliverygroups) {
-    #Get Desktops in each deliverly group that are tagged already with DRAIN
-    $taggedserver = @()
-    $taggedserver = Get-BrokerDesktop -AdminAddress $DDC -DesktopGroupName $deliverygroup.Name -MaxRecordCount 10000 -Tag "DRAIN*"
-
-    if ($taggedserver -eq $null) {
-        
-        $target = Get-BrokerDesktop -AdminAddress $DDC -MaxRecordCount 10000 -DesktopGroupName $deliverygroup.name -Filter { InMaintenanceMode -eq $false } | sort LastDeregistrationTime | Select -first 1
-        
-        if ($target -ne $null) {
-            #update tag for DRAIN and date/time machine was placed in maintenance mode by script
-            $date = (Get-Date).toString("yyyy-MM-dd HH,mm,ss")
-            New-BrokerTag -AdminAddress $ddc -Name "DRAIN - $date - $($Target.HostedMachineName)" | Add-BrokerTag -Machine $target.MachineName
-                
-            #Place server in maintenance mode
-            $target.MachineName | Set-BrokerMachineMaintenanceMode -adminAddress $DDC -MaintenanceMode $true
-            #Write-Host "Tagging: " + $target.machinename
-        }
+    }
+    catch {RETURN}
+    
+    $day = (Get-Date).DayOfWeek
+    
+    if ($day -eq "Saturday" -or $day -eq "Sunday"){
+    Write-Host "Do nothing.  Day is $day"
+    $LastExitCode = 1
+    exit $LASTEXITCODE
+    }
+    
+    #Variables
+    $deliveryGroups = @()
+    
+    $temp = New-Object psobject -Property @{
+    DDC = "YourDDC.Company.com"
+    DeliveryGroup = "DeliveryGroupName"
+    }
+    
+    $deliveryGroups += $temp
+    
+    #This always shows up on the Studio Dashboard as a create - regardless if it already exists
+    foreach ($dg in $deliveryGroups) {
+    if (!(Get-BrokerTag -AdminAddress $dg.DDC -Name "DRAIN")) {
+        New-BrokerTag -AdminAddress $dg.DDC -Name "DRAIN"
     }
     else {
-        $sessions = Get-BrokerSession -AdminAddress $DDC -MaxRecordCount 10000 -MachineName $taggedserver.MachineName
-        if (($taggedserver.InMaintenanceMode -eq $true) -and ($sessions.count -eq 0)) {
-                #Remove tag
-                $removeTag = Get-BrokerTag -AdminAddress $ddc -MachineUid $taggedserver.MachineUid
-                Remove-BrokerTag -AdminAddress $DDC -Machine $taggedserver.MachineName $Removetag
-                Remove-BrokerTag -AdminAddress $DDC $Removetag
-
-                #Remove from maintenance mode
-                $taggedserver.MachineName | Set-BrokerMachineMaintenanceMode -adminAddress $DDC -MaintenanceMode $false
-
-                #reboot server
-                #Write-Host "This is where I would reboot: " $taggedServer.DNSName   
-                New-BrokerHostingPowerAction -adminAddress $DDC -MachineName $taggedserver.MachineName -Action Restart   
+        Write-Host "Tag already exists."
+    }
+    }
+    
+    #for each DG in DGs
+    foreach ($DeliveryGroup in $deliveryGroups) {
+    
+        #get total number of desktops in each DG
+        $desktopsAll = Get-BrokerDesktop -AdminAddress $deliveryGroup.ddc -MaxRecordCount 10000 -DesktopGroupName $DeliveryGroup.DeliveryGroup
+        #calculate the allowed number of VMs in maintenance mode at once
+        #casting as an int, but this is a problem for delivery groups with 2 or less cast as an int rounds down to 0
+        [int]$allowedThreshold = ($desktopsall.count * .20)
+        #get what is tagged for draining
+        $WhatIsTaggedForDraining = $DesktopsAll | where {$_.InMaintenanceMode -eq $true -and $_.tags -contains "DRAIN"}
+    
+        #Max number is tagged and in maint.
+        if ($AllowedThreshold -eq $whatistaggedfordraining.count) {
+            #Look at targets already tagged, can the be rebooted?
+            foreach ($desktop in $WhatIsTaggedForDraining) {
+                If ($desktop.SummaryState -eq "Available"){
+                    
+                    #Remove tag
+                    $removeTag = Get-BrokerTag -AdminAddress $DeliveryGroup.ddc -MachineUid $desktop.MachineUid
+                    Remove-BrokerTag -AdminAddress $DeliveryGroup.ddc -Machine $desktop.MachineName $Removetag
+        
+                    #Remove from maintenance mode
+                    $desktop.MachineName | Set-BrokerMachineMaintenanceMode -adminAddress $DeliveryGroup.DDC -MaintenanceMode $false
+    
+                    #reboot server
+                    #A powered off VM sent a Restart action will power it on. 
+                    New-BrokerHostingPowerAction -adminAddress $DeliveryGroup.DDC -MachineName $desktop.MachineName -Action Restart 
+                }
+            }
+            #making a decision here not to look at placing new servers in maintenance mode during the same cycle.
+            #Because a reboot can take several minutes to process, there is the risk of having greater than 20% unavailable at a time
+        }
+        elseif ($WhatIsTaggedForDraining.count -lt $allowedThreshold) { 
+            #get teh difference.  This will be the number of additional machines we can place in maintenance mode
+            $difference = ($allowedThreshold - $whatistaggedfordraining.count)
+    
+            $toBe = Get-BrokerDesktop -AdminAddress $DeliveryGroup.ddc -MaxRecordCount 10000 -DesktopGroupName $DeliveryGroup.DeliveryGroup | `
+                        Where {$_.InMaintenanceMode -eq $false} | `
+                        sort LastDeregistrationTime | `
+                        Select -first $difference
+    
+            #tag new servers based on the amount tobe within allowed threshold
+            $target = Get-BrokerDesktop -AdminAddress $DeliveryGroup.DDC -MaxRecordCount 10000 -DesktopGroupName $DeliveryGroup.DeliveryGroup -Filter { InMaintenanceMode -eq $false } | sort LastDeregistrationTime | Select -first $toBe.count
+        
+            if ($target -ne $null) {
+                #update tag for DRAIN and date/time machine was placed in maintenance mode by script
+                foreach ($item in $target) {
+                
+                Get-BrokerTag -AdminAddress $DeliveryGroup.DDC -Name "DRAIN" | Add-BrokerTag -Machine $item.MachineName
+                    
+                #Place server in maintenance mode
+                $item.MachineName | Set-BrokerMachineMaintenanceMode -adminAddress $DeliveryGroup.DDC -MaintenanceMode $true
+                }
+            }
+            #get this value again that will include newly tagged           
+            $WhatIsTaggedForDraining = Get-BrokerDesktop -AdminAddress $DeliveryGroup.ddc -MaxRecordCount 10000 -DesktopGroupName $DeliveryGroup.DeliveryGroup | where {$_.InMaintenanceMode -eq $true -and $_.tags -contains "DRAIN"}
+    
+            #let's check if we can reboot anything.
+            foreach ($desktop in $WhatIsTaggedForDraining) {
+                If ($desktop.SummaryState -eq "Available"){
+                    
+                    #Remove tag
+                    $removeTag = Get-BrokerTag -AdminAddress $DeliveryGroup.ddc -MachineUid $desktop.MachineUid
+                    Remove-BrokerTag -AdminAddress $DeliveryGroup.ddc -Machine $desktop.MachineName $Removetag
+        
+                    #Remove from maintenance mode
+                    $desktop.MachineName | Set-BrokerMachineMaintenanceMode -adminAddress $DeliveryGroup.DDC -MaintenanceMode $false
+    
+                    #reboot server
+                    #A powered off VM sent a Restart action will power it on. 
+                    New-BrokerHostingPowerAction -adminAddress $DeliveryGroup.DDC -MachineName $desktop.MachineName -Action Restart 
+                }
+            }
+        }
+        else {
+            Write-host "Default exit"
         }
     }
-}
